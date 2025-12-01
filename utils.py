aff61498f297ecd97ecae70578d7e329519df78b
proyectout2025-dot <proyectout2025@gmail.com>
2025-12-01 17:52:31 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
-from pydrive2.auth import GoogleAuth
-from pydrive2.drive import GoogleDrive
-import base64
 
+# -----------------------------------------------------
+# OBTENER HOJA DE GOOGLE SHEETS
+# -----------------------------------------------------
 def get_gsheet(sheet_name):
     creds_dict = st.secrets["google_service_account"]
+
     creds = Credentials.from_service_account_info(
         creds_dict,
         scopes=[
@@ -14,6 +15,16 @@ def get_gsheet(sheet_name):
             "https://www.googleapis.com/auth/drive"
         ],
     )
+
     client = gspread.authorize(creds)
+
     sheet = client.open_by_url(st.secrets["sheets"]["sheet_url"])
+
     return sheet.worksheet(sheet_name)
+
+# -----------------------------------------------------
+# LEER HOJA COMPLETA
+# -----------------------------------------------------
+def read_sheet(sheet_name):
+    ws = get_gsheet(sheet_name)
+    return ws.get_all_records()
