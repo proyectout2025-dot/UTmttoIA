f5f291319a1dad46687ee6bdfd08089d1921d891
proyectout2025-dot <proyectout2025@gmail.com>
2025-12-01 17:44:24 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,71 +1,19 @@
+import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
-import pandas as pd
-from openai import OpenAI
-import streamlit as st
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 import base64
-import PyPDF2
-import io
 
-# -------------------------
-# GOOGLE AUTH
-# -------------------------
 def get_gsheet(sheet_name):
-    secrets = st.secrets["google"]
-    scope = [
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
-    ]
-
-    credentials = Credentials.from_service_account_info(secrets, scopes=scope)
-    client = gspread.authorize(credentials)
+    creds_dict = st.secrets["google_service_account"]
+    creds = Credentials.from_service_account_info(
+        creds_dict,
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+        ],
+    )
+    client = gspread.authorize(creds)
     sheet = client.open_by_url(st.secrets["sheets"]["sheet_url"])
     return sheet.worksheet(sheet_name)
-
-
-# -------------------------
-# LECTURA DE SHEETS
-# -------------------------
-def read_sheet(sheet_name):
-    ws = get_gsheet(sheet_name)
-    data = ws.get_all_records()
-    return pd.DataFrame(data)
-
-
-# -------------------------
-# ESCRITURA A SHEETS
-# -------------------------
-def append_sheet(sheet_name, row_dict):
-    ws = get_gsheet(sheet_name)
-    ws.append_row(list(row_dict.values()))
-
-
-# -------------------------
-# SUBIR ARCHIVOS A DRIVE
-# -------------------------
-def upload_file_to_drive(file, filename):
-    import googleapiclient.discovery
-    from googleapiclient.http import MediaIoBaseUpload
-
-    secrets = st.secrets["google"]
-    credentials = Credentials.from_service_account_info(
-        secrets,
-        scopes=["https://www.googleapis.com/auth/drive"]
-    )
-
-    service = googleapiclient.discovery.build("drive", "v3", credentials=credentials)
-
-    file_metadata = {
-        "name": filename,
-        "parents": [st.secrets["drive"]["folder_id"]]
-    }
-
-    media = MediaIoBaseUpload(file, mimetype="application/pdf", resumable=True)
-
-    uploaded = service.files().create(
-        body=file_metadata,
-        media_body=media,
-        fields="id, webViewLink, webContentLink"
-    ).execute()
-
-    return uploaded
