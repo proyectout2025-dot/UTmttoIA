5e58d2c96d8dcf331ede6be72ecf5fa25303cdc0
proyectout2025-dot <proyectout2025@gmail.com>
2025-12-02 19:17:36 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,62 +1,43 @@
+import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
-# -----------------------------------------------------------
-# CONFIGURACIÓN DE GOOGLE SHEETS
-# -----------------------------------------------------------
-def get_client():
+
+# -----------------------------------
+# Conexión a Google Sheets
+# -----------------------------------
+def _get_client():
+    creds_info = st.secrets["gcp_service_account"]
+
     scopes = [
         "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
+        "https://www.googleapis.com/auth/drive",
     ]
-    credentials = Credentials.from_service_account_file("service_account.json", scopes=scopes)
-    return gspread.authorize(credentials)
 
-# -----------------------------------------------------------
-# LEER UNA HOJA DE CÁLCULO
-# -----------------------------------------------------------
-def read_sheet(sheet_name, worksheet_name):
-    client = get_client()
-    sheet = client.open(sheet_name)
-    worksheet = sheet.worksheet(worksheet_name)
-    return worksheet.get_all_records()
+    credentials = Credentials.from_service_account_info(creds_info, scopes=scopes)
+    client = gspread.authorize(credentials)
+    return client
 
-# -----------------------------------------------------------
-# AGREGAR DATOS A UNA HOJA
-# -----------------------------------------------------------
-def append_sheet(sheet_name, worksheet_name, values):
-    client = get_client()
-    sheet = client.open(sheet_name)
-    worksheet = sheet.worksheet(worksheet_name)
-    worksheet.append_row(values)
 
-# -----------------------------------------------------------
-# SUBIR ARCHIVO A GOOGLE DRIVE
-# -----------------------------------------------------------
-def upload_file_to_drive(file, drive_folder_id):
-    from googleapiclient.discovery import build
-    from googleapiclient.http import MediaIoBaseUpload
-    import io
+# -----------------------------------
+# Leer hoja completa
+# -----------------------------------
+def read_sheet(spreadsheet_name, worksheet_name):
+    client = _get_client()
+    sh = client.open(spreadsheet_name)
+    ws = sh.worksheet(worksheet_name)
+    return ws.get_all_records()
 
-    scopes = ["https://www.googleapis.com/auth/drive"]
-    credentials = Credentials.from_service_account_file("service_account.json", scopes=scopes)
-    service = build("drive", "v3", credentials=credentials)
 
-    file_metadata = {
-        "name": file.name,
-        "parents": [drive_folder_id],
-    }
+# -----------------------------------
+# Escribir una fila nueva
+# -----------------------------------
+def append_sheet(spreadsheet_name, worksheet_name, row_dict):
+    client = _get_client()
+    sh = client.open(spreadsheet_name)
+    ws = sh.worksheet(worksheet_name)
 
-    media = MediaIoBaseUpload(
-        io.BytesIO(file.getvalue()),
-        mimetype=file.type,
-        resumable=True
-    )
+    headers = ws.row_values(1)
 
-    uploaded_file = service.files().create(
-        body=file_metadata,
-        media_body=media,
-        fields="id"
-    ).execute()
-
-    return uploaded_file.get("id")
+    row = [row_dict.get(h, "") for h in headers]
+    ws.append_row(row)
